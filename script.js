293cac9b7229192dfdb77f9916709cdf0f460487
sagmaar <47426476+sagmaar@users.noreply.github.com>
2022-07-20 11:03:42 +0100

    Create script.js
    Script added

---
+++ script.js
@@ -0,0 +1,11 @@
+<script src="jquery-1.11.3.min.js"></script>
+<script>
+   $(window).load(function() {
+     $("#loading").fadeOut(500);
+   });
+</script><script src="jquery-1.11.3.min.js"></script>
+<script>
+   $(window).load(function() {
+     $("#loading").fadeOut(500);
+   });
+</script>
